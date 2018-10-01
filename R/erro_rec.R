97e5d0f9516269fdbfe07f2b1ab2fc6d05c79c73
leo-filgueira <leonardo_filgueira@id.uff.br>
2018-10-01 20:11:55 -0300

    Calcula erro para bases com diferentes números de clusters

--- R/erro_rec.R
+++ R/erro_rec.R
@@ -1,6 +1,8 @@
 ## Erro com todo mundo
 
-teste <- as(dados, "realRatingMatrix")
+teste <- as(dados %>% 
+              select(-contains("cluster")) %>% 
+              as.data.frame(), "realRatingMatrix")
 
 min(rowCounts(teste))
 
@@ -10,100 +12,114 @@ model <- getData(model_train_scheme, "train") %>% #only fit on the 70% training 
   Recommender(method = "UBCF", parameter = list(method = "pearson"))
 
 model_pred <- predict(model, getData(model_train_scheme, "known"), type = "ratings")
-test_error <- calcPredictionAccuracy(model_pred, getData(model_train_scheme, "unknown"), byUser = TRUE)
+test_error <- calcPredictionAccuracy(model_pred, getData(model_train_scheme, "unknown"), byUser = F)
 
 head(test_error)
 summary(test_error)
 
-boxplot(test_error, main = "Todos os usuários")
+# boxplot(test_error, main = "Todos os usuários")
 
 ## Erro com clusters
 
-# Cluster 1
-cluster1 <- cluster1 %>% 
-  select(-cluster) %>% 
-  as.data.frame()
-  
-teste <- as(cluster1, "realRatingMatrix")
+# # Cluster 1
+# cluster1 <- cluster1 %>% 
+#   select(-cluster) %>% 
+#   as.data.frame()
+#   
+# teste <- as(cluster1, "realRatingMatrix")
+# 
+# min(rowCounts(teste))
+# 
+# model_train_scheme <- evaluationScheme(teste, train = 0.7, goodRating = 4, given = 18)
+# 
+# model1 <- getData(model_train_scheme, "train") %>% #only fit on the 75% training data.
+#   Recommender(method = "UBCF", parameter = list(method = "pearson"))
+# 
+# model1_pred <- predict(model1, getData(model_train_scheme, "known"), type = "ratings")
+# test_error_cl1 <- calcPredictionAccuracy(model1_pred, getData(model_train_scheme, "unknown"), byUser = TRUE)
+# 
+# head(test_error_cl1)
+# summary(test_error_cl1)
+# 
+# # Cluster 2
+# cluster2 <- cluster2 %>% 
+#   select(-cluster) %>% 
+#   as.data.frame()
+# 
+# teste <- as(cluster2, "realRatingMatrix")
+# 
+# min(rowCounts(teste))
+# 
+# model_train_scheme <- evaluationScheme(teste, train = 0.7, goodRating = 4, given = 18)
+# 
+# model2 <- getData(model_train_scheme, "train") %>% #only fit on the 75% training data.
+#   Recommender(method = "UBCF", parameter = list(method = "pearson"))
+# 
+# model2_pred <- predict(model2, getData(model_train_scheme, "known"), type = "ratings")
+# test_error_cl2 <- calcPredictionAccuracy(model2_pred, getData(model_train_scheme, "unknown"), byUser = TRUE)
+# 
+# head(test_error_cl2)
+# summary(test_error_cl2)
+# 
+# # Cluster 3
+# cluster3 <- cluster3 %>% 
+#   select(-cluster) %>% 
+#   as.data.frame()
+# 
+# teste <- as(cluster3, "realRatingMatrix")
+# 
+# min(rowCounts(teste))
+# 
+# model_train_scheme <- evaluationScheme(teste, train = 0.7, goodRating = 4, given = 18)
+# 
+# model3 <- getData(model_train_scheme, "train") %>% #only fit on the 75% training data.
+#   Recommender(method = "UBCF", parameter = list(method = "pearson"))
+# 
+# model3_pred <- predict(model3, getData(model_train_scheme, "known"), type = "ratings")
+# test_error_cl3 <- calcPredictionAccuracy(model3_pred, getData(model_train_scheme, "unknown"), byUser = TRUE)
+# 
+# head(test_error_cl3)
+# summary(test_error_cl3)
+# 
+# # Cluster 4
+# cluster4 <- cluster4 %>% 
+#   select(-cluster) %>% 
+#   as.data.frame()
+# 
+# teste <- as(cluster4, "realRatingMatrix")
+# 
+# min(rowCounts(teste))
+# 
+# model_train_scheme <- evaluationScheme(teste, train = 0.7, goodRating = 4, given = 18)
+# 
+# model4 <- getData(model_train_scheme, "train") %>% #only fit on the 75% training data.
+#   Recommender(method = "UBCF", parameter = list(method = "pearson"))
+# 
+# model4_pred <- predict(model4, getData(model_train_scheme, "known"), type = "ratings")
+# test_error_cl4 <- calcPredictionAccuracy(model4_pred, getData(model_train_scheme, "unknown"), byUser = TRUE)
+# 
+# head(test_error_cl4)
+# summary(test_error_cl4)
+# 
+# erro_cluster <- rbind(test_error_cl1, test_error_cl2, 
+#                       test_error_cl3, test_error_cl4)
+# 
+# par(mar = c(2, 2.5, 1.55, 1), mfrow = c(2, 1))
+# boxplot(test_error, main = "Todos os usuários")
+# boxplot(erro_cluster, main = "Clusters")  
+# 
+# par(mfrow = c(2, 2), mar = c(2, 2.5, 1.55, 1))
 
-min(rowCounts(teste))
+c2 <- erro_cluster(dados, cluster_2)
+c2
 
-model_train_scheme <- evaluationScheme(teste, train = 0.7, goodRating = 4, given = 18)
+c3 <- erro_cluster(dados, cluster_3)
+c3
 
-model1 <- getData(model_train_scheme, "train") %>% #only fit on the 75% training data.
-  Recommender(method = "UBCF", parameter = list(method = "pearson"))
+c4 <- erro_cluster(dados, cluster_4)
+c4
 
-model1_pred <- predict(model1, getData(model_train_scheme, "known"), type = "ratings")
-test_error_cl1 <- calcPredictionAccuracy(model1_pred, getData(model_train_scheme, "unknown"), byUser = TRUE)
-
-head(test_error_cl1)
-summary(test_error_cl1)
-
-# Cluster 2
-cluster2 <- cluster2 %>% 
-  select(-cluster) %>% 
-  as.data.frame()
-
-teste <- as(cluster2, "realRatingMatrix")
-
-min(rowCounts(teste))
-
-model_train_scheme <- evaluationScheme(teste, train = 0.7, goodRating = 4, given = 18)
-
-model2 <- getData(model_train_scheme, "train") %>% #only fit on the 75% training data.
-  Recommender(method = "UBCF", parameter = list(method = "pearson"))
-
-model2_pred <- predict(model2, getData(model_train_scheme, "known"), type = "ratings")
-test_error_cl2 <- calcPredictionAccuracy(model2_pred, getData(model_train_scheme, "unknown"), byUser = TRUE)
-
-head(test_error_cl2)
-summary(test_error_cl2)
-
-# Cluster 3
-cluster3 <- cluster3 %>% 
-  select(-cluster) %>% 
-  as.data.frame()
-
-teste <- as(cluster3, "realRatingMatrix")
-
-min(rowCounts(teste))
-
-model_train_scheme <- evaluationScheme(teste, train = 0.7, goodRating = 4, given = 18)
-
-model3 <- getData(model_train_scheme, "train") %>% #only fit on the 75% training data.
-  Recommender(method = "UBCF", parameter = list(method = "pearson"))
-
-model3_pred <- predict(model3, getData(model_train_scheme, "known"), type = "ratings")
-test_error_cl3 <- calcPredictionAccuracy(model3_pred, getData(model_train_scheme, "unknown"), byUser = TRUE)
-
-head(test_error_cl3)
-summary(test_error_cl3)
-
-# Cluster 4
-cluster4 <- cluster4 %>% 
-  select(-cluster) %>% 
-  as.data.frame()
-
-teste <- as(cluster4, "realRatingMatrix")
-
-min(rowCounts(teste))
-
-model_train_scheme <- evaluationScheme(teste, train = 0.7, goodRating = 4, given = 18)
-
-model4 <- getData(model_train_scheme, "train") %>% #only fit on the 75% training data.
-  Recommender(method = "UBCF", parameter = list(method = "pearson"))
-
-model4_pred <- predict(model4, getData(model_train_scheme, "known"), type = "ratings")
-test_error_cl4 <- calcPredictionAccuracy(model4_pred, getData(model_train_scheme, "unknown"), byUser = TRUE)
-
-head(test_error_cl4)
-summary(test_error_cl4)
-
-erro_cluster <- rbind(test_error_cl1, test_error_cl2, 
-                      test_error_cl3, test_error_cl4)
-
-par(mar = c(2, 2.5, 1.55, 1), mfrow = c(2, 1))
-boxplot(test_error, main = "Todos os usuários")
-boxplot(erro_cluster, main = "Clusters")  
-
-
+test_error
+colMeans(do.call(rbind, c2))
+colMeans(do.call(rbind, c3))
+colMeans(do.call(rbind, c4))
