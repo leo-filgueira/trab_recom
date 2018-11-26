e5e5842dd29dec05e12ae87313fa3154a4ae1d62
leo-filgueira <leonardo_filgueira@id.uff.br>
2018-11-25 22:18:52 -0200

    Agrupa usuários usando kmeans

---
+++ R/Cluster_kmeans.R
@@ -0,0 +1,32 @@
+# Cluster com k-means
+
+# Com rating médio
+
+# Imputando NA com média da coluna
+dados2_imputados <- dados2 %>% 
+  mutate_all(function(x){case_when(
+    is.na(x) ~ mean(x, na.rm = T),
+    T ~ x
+  )}
+  ) 
+
+for(i in 2:15){
+  assign(paste0("kmeans_", i, '_clusters_rating'), 
+         kmeans(dados2_imputados, i, iter.max = 20))
+}
+
+## Com proporção de filmes assistidos
+
+# Imputando NA
+dados3_imputados <- dados3 %>% 
+  mutate_all(function(x){case_when(
+    is.na(x) ~ mean(x, na.rm = T),
+    T ~ x
+  )}
+  ) 
+
+for(i in 2:15){
+  assign(paste0("kmeans_", i, '_clusters_prop'), 
+         kmeans(dados3_imputados, i, iter.max = 20))
+}
+
